eaa3068d9e4c1ebda802f51d321b5064b6c89837
Nonju <albinzon@gmail.com>
2018-05-18 15:21:22 +0200

    LedReceiver receives commands and controls basic led - PWM when number - On / off when "ON" / "OFF"

--- btLedReceiver/receiver/receiver.ino
+++ btLedReceiver/receiver/receiver.ino
@@ -1,10 +1,14 @@
 #include <SoftwareSerial.h>
+#include <ctype.h>
 
 // Connect the HC-05 TX to Arduino pin 2 RX
 // Connect the HC-05 RX to Aruduino pin 3 TX ( Through Voltage Divider!! )
 SoftwareSerial BTserial(2, 3); // RX | TX
 
+# define ledPin 5
+
 char c;
+String command;
 
 void setup() {
   // Serial setup
@@ -14,12 +18,46 @@ void setup() {
 
   // BT serial setup
   BTserial.begin(38400); // HC-05 default serial speed for AT mode set to 38400
+
+  // Setup pins
+  pinMode(ledPin, OUTPUT);
+}
+
+bool stringIsInt(String s) {
+  for (int i = 0; i < s.length(); ++i) {
+    char c = s.charAt(i);
+    if (!isdigit(c)) return false;
+  }
+  return true;
+}
+
+void executeCommand() {
+  Serial.println("Executing command: " + command);
+  if (stringIsInt(command)) {
+    int number = command.toInt();
+    if (number > 255) number = 255;
+    if (number < 0) number = 0;
+    analogWrite(ledPin, number);
+  }
+  else if (command.equals("ON")) digitalWrite(ledPin, HIGH);
+  else if (command.equals("OFF")) digitalWrite(ledPin, LOW);
+  else Serial.println("Unknown command");
+}
+
+void handleInput() {
+  if (c == ';') {
+    executeCommand();
+    command = "";
+    return;
+  }
+  command.concat(c);
 }
 
 void loop() {
   // Keep reading from HC-05 serial and send to Arduino Serial Monitor
   if (BTserial.available()) {
     c = BTserial.read();
+    handleInput();
     Serial.write(c);
   }
 

adb25365ca8f3d9387394fb4132a2b2f5e2c7187
Nonju <albinzon@gmail.com>
2018-05-27 22:17:53 +0200

    Updated postinstall scripts to fit new bt lib

--- btLED/scripts/updateReactNativeBleGradle.py
+++ btLED/scripts/updateReactNativeBleGradle.py
@@ -1,6 +1,6 @@
 
-# react-native-ble-plx build gradle path (relative to root)
-FILE_PATH = './node_modules/react-native-ble-plx/android/build.gradle'
+# easy-bluetooth-classic build gradle path (relative to root)
+FILE_PATH = './node_modules/easy-bluetooth-classic/android/build.gradle'
 
 def compileToImplementation():
 	content = ''
@@ -10,14 +10,16 @@ def compileToImplementation():
 	for i in range(0, len(content)):
 		line = content[i]
 		line = line.replace('compile ', 'implementation ')
+		line = line.replace('compile(', 'implementation(')
 		line = line.replace('testCompile ', 'testImplementation ')
+		line = line.replace('testCompile(', 'testImplementation(')
 		content[i] = line
 
 	with open(FILE_PATH, 'w') as f:
 		f.write(''.join(content))
 
 def main():
-	print('Before File update')
+	print('Updating easy-bluetooth-classic build.gradle')
 	compileToImplementation()
 
 if __name__ == '__main__':
